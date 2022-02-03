c0c9c0816190d5fa0ee1b743d5997782f4109ab3
Faizal Azman <faizalazman88@gmail.com>
2022-02-03 16:07:16 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,47 +2,48 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require('cors')
+app.use(cors({ optionsSuccessStatus: 200 })) // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html')
+})
 
-
-// your first API endpoint... 
-app.get("/api/:date?", function (req, res) {
+// your first API endpoint...
+app.get('/api/:date?', function (req, res) {
   if (!req.params.date) {
-    const currentDate = new Date();
+    const currentDate = new Date()
     res.status(200).json({
-      "unix": currentDate.valueOf(),
-      "utc": currentDate.toUTCString()
+      unix: currentDate.valueOf(),
+      utc: currentDate.toUTCString(),
     })
   } else {
-    const date = new Date(isNaN(req.params.date) ? req.params.date : parseInt(req.params.date));
-    if (!Date.parse(req.params.date)) {
-      res.status(400).json({
-        "error": "Invalid Date"
+    const date = new Date(
+      isNaN(req.params.date) ? req.params.date : parseInt(req.params.date),
+    )
+    if (Date.parse(req.params.date)) {
+      res.status(200).json({
+        unix: date.valueOf(),
+        utc: date.toUTCString(),
       })
     } else {
-    res.status(200).json({
-      "unix": date.valueOf(),
-      "utc": date.toUTCString()
-    })
+      res.status(400).json({
+        error: 'Invalid Date',
+      })
+    }
   }
-  }
-});
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+  console.log('Your app is listening on port ' + listener.address().port)
+})
